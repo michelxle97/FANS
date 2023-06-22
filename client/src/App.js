6da9a24b16146ce57248bc6e91bb2d1f5647e977
vboxuser <vboxuser@UbuntuBox.myguest.virtualbox.org>
2023-06-22 12:29:34 -0400

    created guess that pokemon game

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import GuessPokemon from './components/GuessPokemon';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/257.png"} className="App-logo mask brightness" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <GuessPokemon/>
         <a
           className="App-link"
           href="https://reactjs.org"
