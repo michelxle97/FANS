ce0c726c81583f751af936bf32260ed954ca750a
Matthew <isikl3gendi@gmail.com>
2023-06-23 10:19:25 -0500

    added detail component

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,27 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import GuessPokemon from './components/GuessPokemon';
 import PokemonByType from './components/PokemonByType';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import PokemonDetails from './components/PokemonDetails';
 
 
 function App() {
 
   return (
-    <div>
-      <Navbar />
-      <GuessPokemon />
-      <PokemonByType />
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<GuessPokemon />} />
+          <Route path="/pokemon" element={<PokemonByType />} />
+          <Route path="/pokemonDetails/:name" element={<PokemonDetails />} />
+        </Routes>
+      </div>
+    </Router>
+
+
+
+
 
   );
 }
---
+++ client/src/components/PokemonDetails.js
@@ -0,0 +1,58 @@
+import { useEffect, useState } from 'react';
+import { useParams, Link, useLocation } from 'react-router-dom';
+import './PokemonDetails.css';
+
+const PokemonDetails = () => {
+
+    const { name } = useParams();
+    const [pokemonDetails, setPokemonDetails] = useState(null);
+    const location = useLocation();
+
+
+    useEffect(() => {
+        const fetchPokemonDetails = async () => {
+            try {
+                const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+                const data = await response.json();
+                setPokemonDetails(data);
+            } catch (error) {
+                console.error(`Error fetching Pokémon details for ${name}:`, error);
+            }
+        };
+
+        fetchPokemonDetails();
+    }, [name]);
+
+    if (!pokemonDetails) {
+        return <div>Loading...</div>;
+    }
+    const { weight, sprites, height, base_experience, types } = pokemonDetails;
+
+
+    return (
+        <div className='container' id='container'>
+            <h3>{name.charAt(0).toUpperCase() + name.slice(1)}</h3>
+            <p>Weight: {weight}</p>
+            <p>Height in decimeters: {height} </p>
+            <p>Base XP: {base_experience}</p>
+            <p>Type(s): {pokemonDetails.types.map((type) => type.type.name + " | ")}</p>
+
+            <img src={sprites.front_default} alt={name} />
+            <img src={sprites.back_default} alt={name} />
+
+            <Link
+                to={{
+                    pathname: '/pokemon'
+                }}
+            >
+                Go Back to Pokemon List
+            </Link>
+
+
+        </div>
+    );
+
+}
+
+export default PokemonDetails;
+
