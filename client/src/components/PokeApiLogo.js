6724fccb67daf44951e80bf127244e9dcfb7ac7c
vboxuser <vboxuser@UbuntuBox.myguest.virtualbox.org>
2023-06-23 11:18:34 -0400

    added new homescreen, background, and carousel

---
+++ client/src/components/PokeApiLogo.js
@@ -0,0 +1,15 @@
+import './PokeApiLogo.css'
+
+function PokeApiLogo() {
+    return(
+        <div className="banner">
+            <div className="inner-banner">
+                <div className="inner-image">
+                    <img alt="PokéAPI" src="https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png"></img>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default PokeApiLogo;
